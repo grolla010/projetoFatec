1e0483ac83982c6f529f28e699f81697ab874f91
grolla010 <163444945+grolla010@users.noreply.github.com>
2025-10-13 20:34:12 -0300

    Create Sistema

---
+++ sistema/admin/login.php
@@ -0,0 +1,52 @@
+<?php
+
+include("./config.inc.php");
+include("./session.php");
+include("../header.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (contaValida($_POST["username"], $_POST["password"])) {
+        registraConta($_POST["username"]);
+        header("Location: /sistema/admin/index.php");
+        exit;
+    }
+    $username = $_POST["username"];
+    $mensagem = "Username ou Password incorreto!";
+}
+
+?>
+
+<div class="login-container">
+    <form name="formLogin" method="POST" class="login-form">
+
+        <h2>LOGIN</h2>
+
+        <div class="error-message">
+            <?=isset($mensagem)?$mensagem:"&nbsp;";?>
+        </div>
+
+        <div class="input-group">
+            <label for="username">Usuário</label>
+            <input type="text" id="username" name="username" value="<?=isset($username)?$username:"";?>" required>
+        </div>
+
+        <div class="input-group">
+            <label for="password">Senha</label>
+            <input type="password" id="password" name="password" value="<?=isset($password)?$password:"";?>" required>
+        </div>
+
+        <button type="submit" name="submit">Entrar</button>
+
+        <div class="register-link">
+            <p>Não tem uma conta? <a href="/sistema/admin/register.php">Registre-se</a></p>
+        </div>
+
+    </form>
+</div>
+
+<script language="JavaScript" type="text/javascript">
+    //</script>
+
+<?php
+include("../footer.php");
+?>
---
+++ sistema/header.php
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+	<title>Fatec Bauru</title>
+	<link rel="stylesheet" href="../style.css">
+</head>
+
+<body style="background-color: <?= $bodyBG; ?>;">
+	<img src="/sistema/fatecbauru.jpg" />
+	<h1>Fatec Bauru</h1>	
