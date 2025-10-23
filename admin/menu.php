51146aa40ad5233fd30d1f7a763819b8abafc9aa
Mateus Grolla <mateus.grolla@nexum.com.br>
2025-10-23 09:51:36 -0300

    Update menu.php
    Fit: incluindo caminho para categoria

--- admin/menu.php
+++ admin/menu.php
@@ -1,3 +1,4 @@
 <a href="/sistema/admin/index.php" style="color: black;">Index</a>
 <a href="/sistema/admin/prod" style="color: black;">Produtos</a>
+<a href="/sistema/admin/category" style="color: black;">Categorias</a>
 <a href="/sistema/admin/logout.php" style="color: black;">Logout</a>
